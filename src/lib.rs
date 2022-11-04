b1975887cbc4a79ce331f5e49fd9c624257d7d92
parK-dev <kirk.paradis@gmail.com>
2022-11-04 22:54:13 +0900

    Refactor using iterators and closures

--- src/lib.rs
+++ src/lib.rs
@@ -14,12 +14,19 @@ pub struct Config {
 }
 
 impl Config {
-    pub fn new(args: &[String]) -> Result<Self, &'static str> {
+    pub fn new(mut args: std::env::Args) -> Result<Self, &'static str> {
+        args.next();
         if args.len() < 3 {
             return Err("use 'cargo run <query> <filename>'");
         }
-        let query = args[1].clone();
-        let filename = args[2].clone();
+        let query = match args.next() {
+            Some(arg) => arg,
+            None => return Err("Didn't get a query string"),
+        };
+        let filename = match args.next() {
+            Some(arg) => arg,
+            None => return Err("Didn't get a filename"),
+        };
         let case = match env::var("CASE_INSENSITIVE").is_err() {
             true => Case::Sensitive,
             false => Case::Insensitive,
@@ -45,17 +52,10 @@ pub fn run(config: Config) -> Result<(), Box<dyn Error>> {
 }
 
 pub fn search<'a>(query: &str, contents: &'a str) -> Vec<&'a str> {
-    let mut results = Vec::new();
-
-    for line in contents.lines() {
-        if line.contains(query) {
-            results.push(line);
-        }
-    }
-
-    println!("Number of results: {}", results.len());
-
-    results
+    contents
+        .lines()
+        .filter(|line| line.contains(query))
+        .collect()
 }
 
 pub fn search_case_insensitive<'a>(query: &str, contents: &'a str) -> Vec<&'a str> {
